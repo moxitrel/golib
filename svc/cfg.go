9da86eaec79a094632bfc1faf79995566436974f
M2 <6856151+moxitrel@users.noreply.github.com>
2018-08-31 22:14:14 +0800

    set pool min to 2

--- svc/cfg.go
+++ svc/cfg.go
@@ -6,7 +6,7 @@ import (
 )
 
 var (
-	POOL_MIN     uint16 = 0
+	POOL_MIN     uint16 = 2
 	POOL_MAX     uint16 = math.MaxUint16
 	POOL_DELAY          = 200 * time.Millisecond
 	POOL_TIMEOUT        = time.Minute
