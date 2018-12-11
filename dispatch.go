7db78196cbfcef97926369975961bbe8451e22b0
M2 <6856151+moxitrel@users.noreply.github.com>
2018-12-11 21:32:39 +0800

    Add DispatchMessage interface

---
+++ dispatch.go
@@ -0,0 +1,31 @@
+package golib
+
+import "unsafe"
+
+// zero value is not a valid key
+type DispatchKey struct {
+	dispatcher unsafe.Pointer
+	key        uintptr
+}
+
+type DispatchMessage interface {
+	DispatchKey() interface{}
+}
+
+type DispatchKeyMessage struct {
+	dispatchKey DispatchKey
+	message     interface{}
+}
+
+func WithDispatchKey(key DispatchKey, message interface{}) DispatchKeyMessage {
+	return DispatchKeyMessage{
+		dispatchKey: key,
+		message:     message,
+	}
+}
+func (o DispatchKeyMessage) DispatchKey() interface{} {
+	return o.dispatchKey
+}
+func (o DispatchKeyMessage) Message() interface{} {
+	return o.message
+}
